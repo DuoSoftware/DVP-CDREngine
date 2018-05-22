41be89375c1fb4325ce2aa5c7c377755ca8af512
dinusha_dck <dinusha@duosoftware.com>
2018-05-22 11:13:37 +0530

    update env variables

--- config/default.js
+++ config/default.js
@@ -48,16 +48,6 @@ module.exports = {
     "vhost":'/'
   },
 
-  "Mongo":
-  {
-    "ip":"104.236.231.11",
-    "port":"27017",
-    "dbname":"dvpdb",
-    "password":"DuoS123",
-    "user":"duo",
-    "replicaset" :"104.236.231.11"
-  },
-
   "Services":
   {
 
@@ -72,7 +62,6 @@ module.exports = {
     "Port":9093,
     "Version":"1.0.0.0"
   },
-  "AbandonCallThreshold": 5,
-  "SaveRawCDRMongo": "true",
+
   "Token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
 };
