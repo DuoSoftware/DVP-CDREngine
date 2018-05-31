18f38ad7fcaabb5d1d0dd6bca60cc1897d0ec36e
dinusha_dck <dinusha@duosoftware.com>
2018-05-31 16:48:08 +0530

    add default config

--- config/default.js
+++ config/default.js
@@ -3,21 +3,21 @@ module.exports = {
   "DB": {
     "Type":"postgres",
     "User":"duo",
-    "Password":"",
+    "Password":"DuoS123",
     "Port":5432,
-    "Host":"",
-    "Database":""
+    "Host":"104.236.231.11",
+    "Database":"duo"
   },
 
   "Redis":
   {
     "mode":"instance",//instance, cluster, sentinel
-    "ip": "",
+    "ip": "138.197.90.92",
     "port": 6389,
-    "user": "",
-    "password": "",
+    "user": "duo",
+    "password": "DuoS123",
     "sentinels":{
-      "hosts": "",
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -27,13 +27,13 @@ module.exports = {
   "Security":
   {
 
-    "ip" : "",
+    "ip" : "45.55.142.207",
     "port": 6389,
-    "user": "",
-    "password": "",
+    "user": "duo",
+    "password": "DuoS123",
     "mode":"sentinel",//instance, cluster, sentinel
     "sentinels":{
-      "hosts": "",
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -41,10 +41,10 @@ module.exports = {
 
   "RabbitMQ":
   {
-    "ip": "",
+    "ip": "45.55.142.207",
     "port": 5672,
-    "user": "",
-    "password": "",
+    "user": "admin",
+    "password": "admin",
     "vhost":'/'
   },
 
@@ -63,5 +63,5 @@ module.exports = {
     "Version":"1.0.0.0"
   },
 
-  "Token":""
+  "Token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
 };
